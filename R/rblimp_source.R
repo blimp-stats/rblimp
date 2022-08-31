255f00aa534bc9859ad62d268efd6f19af703ad5
Brian <bk@austin.utexas.edu>
2022-08-31 11:54:29 -0500

    Change output from .txt to .blimp-out

--- R/rblimp_source.R
+++ R/rblimp_source.R
@@ -27,23 +27,23 @@ rblimp_source <- function(file,
     # Run command
     if(.Platform$OS.type == "unix") {
         if (output == 'all') {
-            exitcode <- system(paste0(cmd,'| tee ', file.path(folder,'output.txt')))
+            exitcode <- system(paste0(cmd,'| tee ', file.path(folder,'output.blimp-out')))
         } else {
-            exitcode <- system(paste(cmd,'--output', file.path(folder,'output.txt')),
+            exitcode <- system(paste(cmd,'--output', file.path(folder,'output.blimp-out')),
                                ignore.stdout = output == 'none')
         }
-        result <- readLines(file.path(folder,'output.txt'))
+        result <- readLines(file.path(folder,'output.blimp-out'))
     } else {
         if (nopowershell) result <- shell(cmd, intern = output == 'none')
         else {
             if (output == 'all') {
-                exitcode <- system(paste0('powershell -command \"', cmd ,' | tee \'', file.path(folder,'output.txt'),'\'\"'))
-                result <- readLines(file.path(folder,'output.txt'),skipNul = TRUE)[-1] # Remove NULL
+                exitcode <- system(paste0('powershell -command \"', cmd ,' | tee \'', file.path(folder,'output.blimp-out'),'\'\"'))
+                result <- readLines(file.path(folder,'output.blimp-out'),skipNul = TRUE)[-1] # Remove NULL
                 result <- result[c(1,seq(0,length(result),2))]
             } else {
-                exitcode <- shell(paste(cmd,'--output', file.path(folder,'output.txt')),
+                exitcode <- shell(paste(cmd,'--output', file.path(folder,'output.blimp-out')),
                                   ignore.stdout = output == 'none')
-                result <- readLines(file.path(folder,'output.txt'))
+                result <- readLines(file.path(folder,'output.blimp-out'))
             }
         }
     }
